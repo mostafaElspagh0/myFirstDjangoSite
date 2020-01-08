4b5aa81fb3e585358345995dccd0a8951493d939
mostafae elspagh <mostafaelspagh0@gmail.com>
2020-01-08 11:04:22 +0200

    register question model in admin page and implement __str__ method for it

--- polls/models.py
+++ polls/models.py
@@ -5,8 +5,10 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_data = models.DateTimeField("date published")
+    def __str__ (self):
+        return self.question_text 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question,on_delete= models.CASCADE)
+    question = models.ForeignKey(Question,on_delete= models.CASCADE,related_name="choices")
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default=0)
